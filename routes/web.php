d820375a1c5ba5ff98febb80f0c869f72c1764b8
Henry <hmingv@qq.com>
2024-11-23 09:06:44 +0800

    feat: code example

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/upload', function (Request $request) {
+    Validator::make($request->all(), [
+        'file' => 'required|file|mimes:cdr,ai|mimetypes:application/coreldraw,application/cdr,application/illustration',
+    ])->validated();
 });
